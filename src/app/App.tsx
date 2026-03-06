cbcf89b19a37ae93c84e6e68339a913d1bbdec42
daniel <chiesdaniel09mk@gmail.com>
2026-03-06 14:07:13 +0100

    \Version sin error 404"

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,73 +1,35 @@
-import { createBrowserRouter, RouterProvider } from "react-router";
-import Home from "./pages/Home";
-import AboutMe from "./pages/AboutMe";
-import Servicios from "./pages/Servicios";
-import Recursos from "./pages/Recursos";
-import Contacto from "./pages/Contacto";
-import AvisoLegal from "./pages/AvisoLegal";
-import PoliticaPrivacidad from "./pages/PoliticaPrivacidad";
-import PoliticaCookies from "./pages/PoliticaCookies";
-import DeclaracionAccesibilidad from "./pages/DeclaracionAccesibilidad";
-import ErrorBoundary from "./components/ErrorBoundary";
+import { HashRouter, Routes, Route } from 'react-router';
+import Home from './pages/Home';
+import AboutMe from './pages/AboutMe';
+import Servicios from './pages/Servicios';
+import Recursos from './pages/Recursos';
+import Contacto from './pages/Contacto';
+import AvisoLegal from './pages/AvisoLegal';
+import PoliticaPrivacidad from './pages/PoliticaPrivacidad';
+import PoliticaCookies from './pages/PoliticaCookies';
+import DeclaracionAccesibilidad from './pages/DeclaracionAccesibilidad';
+import ErrorBoundary from './components/ErrorBoundary';
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    Component: Home,
-    errorElement: <ErrorBoundary />,
-  },
-  {
-    path: "/declaracion-accesibilidad",
-    Component: DeclaracionAccesibilidad,
-    errorElement: <ErrorBoundary />,
-  },
-  {
-    path: "/aviso-legal",
-    Component: AvisoLegal,
-    errorElement: <ErrorBoundary />,
-  },
-  {
-    path: "/politica-privacidad",
-    Component: PoliticaPrivacidad,
-    errorElement: <ErrorBoundary />,
-  },
-  {
-    path: "/politica-cookies",
-    Component: PoliticaCookies,
-    errorElement: <ErrorBoundary />,
-  },
-  {
-    path: "/inicio",
-    Component: Home,
-    errorElement: <ErrorBoundary />,
-  },
-  {
-    path: "/sobre-mi",
-    Component: AboutMe,
-    errorElement: <ErrorBoundary />,
-  },
-  {
-    path: "/servicios",
-    Component: Servicios,
-    errorElement: <ErrorBoundary />,
-  },
-  {
-    path: "/recursos",
-    Component: Recursos,
-    errorElement: <ErrorBoundary />,
-  },
-  {
-    path: "/contacto",
-    Component: Contacto,
-    errorElement: <ErrorBoundary />,
-  },
-  {
-    path: "*",
-    Component: Home,
-    errorElement: <ErrorBoundary />,
-  },
-]);
-
-export default function App() {
-  return <RouterProvider router={router} />;
+function App() {
+  return (
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/inicio" element={<Home />} />
+        <Route path="/sobre-mi" element={<AboutMe />} />
+        <Route path="/about" element={<AboutMe />} />
+        <Route path="/servicios" element={<Servicios />} />
+        <Route path="/recursos" element={<Recursos />} />
+        <Route path="/contacto" element={<Contacto />} />
+        <Route path="/aviso-legal" element={<AvisoLegal />} />
+        <Route path="/politica-privacidad" element={<PoliticaPrivacidad />} />
+        <Route path="/politica-cookies" element={<PoliticaCookies />} />
+        <Route path="/declaracion-accesibilidad" element={<DeclaracionAccesibilidad />} />
+        {/* Ruta fallback */}
+        <Route path="*" element={<Home />} />
+      </Routes>
+    </HashRouter>
+  );
 }
+
+export default App;
